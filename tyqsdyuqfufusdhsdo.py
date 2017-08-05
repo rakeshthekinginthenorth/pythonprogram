c0385aeae5db80bdfbdee4e8355b8389763c0f97
rakeshthekinginthenorth <30741477+rakeshthekinginthenorth@users.noreply.github.com>
2017-08-05 10:24:01 +0530

    created a program for even an odd

--- tyqsdyuqfufusdhsdo.py
+++ tyqsdyuqfufusdhsdo.py
@@ -1,7 +1,5 @@
-num = float(input("Enter a number: "))
-if num > 0:
-   print("Positive number")
-elif num == 0:
-   print("Zero")
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
 else:
-   print("Negative number")
+   print("{0} is Odd".format(num))
